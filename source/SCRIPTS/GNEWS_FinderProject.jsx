ac065a3c58961de0493107d226d18eecc04fa7f8
Rafael Denoni <44037286+rdenoni@users.noreply.github.com>
2025-08-27 14:24:07 -0300

    Auto-sync: file changes

--- source/SCRIPTS/GNEWS_FinderProject.jsx
+++ source/SCRIPTS/GNEWS_FinderProject.jsx
@@ -22,7 +22,7 @@ function getPathDayByDay() {
 
 
 function D9TFindProjectDialog() {
-    var scriptName = 'Finders PROJETOS';
+    var scriptName = 'FIND PROJECTS';
     var SETTINGS_SECTION = "D9TSearch";
 
     var searchPresets = {
@@ -122,12 +122,12 @@ function D9TFindProjectDialog() {
     var searchGrp = win.add('group');
     searchGrp.alignment = 'left';
     
-    var searchBtn = new themeIconButton(searchGrp, { icon: D9T_LENS_ICON || undefined, tips: [(lClick || 'Clique') + ' Finders projetos'] });
+    var searchBtn = new themeIconButton(searchGrp, { icon: D9T_LENS_ICON || undefined, tips: [(lClick || 'Clique') + ' Buscar projetos'] });
     var spacer1 = searchGrp.add('group'); spacer1.preferredSize.width = 1;
     var searchInput = searchGrp.add('edittext'); 
     searchInput.preferredSize.width = 372;
 
-    var placeholderText = 'Digite para Finders...';
+    var placeholderText = 'Digite para Buscar...';
     searchInput.text = placeholderText;
     searchInput.active = false; 
     searchInput.onActivate = function() {
