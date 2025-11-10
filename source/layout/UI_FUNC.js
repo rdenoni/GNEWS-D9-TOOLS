59461d61f90108e5cbcf7ce9ad75aeda1903bd7a
Rafael Denoni <44037286+rdenoni@users.noreply.github.com>
2025-11-10 10:02:11 -0300

    Delete source/layout/UI_FUNC.js

--- source/layout/UI_FUNC.js
+++
@@ -1,9 +0,0 @@
-/*
----------------------------------------------------------------
-> UI_FUNC.js - Aggregador legado (tema + funções principais)
----------------------------------------------------------------
-*/
-
-#include 'theme_api.js'
-#include 'main_ui_functions.js'
-
