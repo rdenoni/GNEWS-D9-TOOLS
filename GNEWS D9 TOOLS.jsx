b755350c7dc6bbcd5459fc6803060d056f1dcbe8
Rafael Denoni <44037286+rdenoni@users.noreply.github.com>
2025-12-03 09:25:37 -0300

    Delete GNEWS D9 TOOLS.jsx

--- GNEWS D9 TOOLS.jsx
+++
@@ -1,9 +0,0 @@
-// GNEWS D9 TOOLS DEV
-try {
-	var scriptMainPath = 'C:/Users/rdenoni/AppData/Roaming/GND9TOOLS script';
-
-	eval('#include \'C:/Users/rdenoni/AppData/Roaming/GND9TOOLS script/GND9TOOLS.jsx\';'); // → UI definition file
-
-} catch (err) {
-	alert('nope... (っ °Д °;)っ\n\n' + err.message);
-}
