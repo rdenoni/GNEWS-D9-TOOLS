12ea517a24bcb1857483bc6fe834dcdd5676c2cb
Rafael Denoni <44037286+rdenoni@users.noreply.github.com>
2025-12-03 09:26:54 -0300

    Delete release directory

--- release/instalacao manual/GND9TOOLS DEV.jsx
+++
@@ -1,9 +0,0 @@
-// GNEWS D9 TOOLS DEV
-try {
-	var scriptMainPath = 'D:/PROJETOS/SCRIPTS AFTER/GNEWS-D9-TOOLS';
-
-	eval('#include \'D:/PROJETOS/SCRIPTS AFTER/GNEWS-D9-TOOLS/GND9TOOLS.jsx\';'); // → UI definition file
-
-} catch (err) {
-	alert('nope... (っ °Д °;)っ\n\n' + err.message);
-}
